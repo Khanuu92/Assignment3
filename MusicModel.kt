1e8734279e477e6eef7ceac2650c1cfde0f4b594
Khanuu92 <106569651+Khanuu92@users.noreply.github.com>
2022-07-01 22:57:24 +0000

    Add files via upload

---
+++ MusicModel.kt
@@ -0,0 +1,15 @@
+package com.example.bilalhamid.model
+
+data class MusicModel (
+    val resultCount: Int,
+    val results: List<ResultMusicModel>
+    )
+
+data class ResultMusicModel(
+    val trackName:String,
+    val artistName:String,
+    val collectionName:String,
+    val artworkUrl100:String,
+    val trackPrice:Double,
+    val previewUrl:String
+)
---
+++ SongDetails.kt
@@ -0,0 +1,46 @@
+package com.example.bilalhamid.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.ImageView
+import android.widget.TextView
+import com.example.bilalhamid.R
+import com.squareup.picasso.Picasso
+
+class SongDetails : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_song_details)
+        val tvCollection: TextView = findViewById(R.id.tvdetailCollectionName)
+        val tvPrice: TextView = findViewById(R.id.tvDetailTrackPrice)
+        val tvArtist: TextView = findViewById(R.id.tvDetailArtist)
+        val tvType: TextView = findViewById(R.id.tvDetailTrack)
+        val imageView: ImageView = findViewById(R.id.ivDetailArtView)
+        val btnPlay:ImageView=findViewById(R.id.btnImage)
+        val bundle: Bundle? =intent.extras
+
+        tvArtist.text = bundle?.getString("aName")
+        tvCollection.text = bundle?.getString("c_name")
+        tvPrice.text=bundle?.getString("price")
+        tvType.text=bundle?.getString("type")
+        Picasso.get()
+            .load(bundle?.getString("KEY"))
+            .placeholder(R.drawable.ic_launcher_foreground)
+
+            .into(imageView)
+
+btnPlay.setOnClickListener{
+
+    run {
+        if (MainActivity.mediaPlayer.isPlaying) {
+            btnPlay.setImageResource(R.drawable.ic_baseline_play_circle_outline_24)
+            MainActivity.mediaPlayer.pause()
+        }else{
+            btnPlay.setImageResource(R.drawable.ic_baseline_pause_circle_outline_24)
+            MainActivity.mediaPlayer.start()
+        }
+
+    }
+}
+    }
+}
